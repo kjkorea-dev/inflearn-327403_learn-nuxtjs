75ca9d37c85a3a3c50efe969e82a58dd7ab958ca
kj-dev <agassi77-dev@gmail.com>
2022-09-01 16:23:10 +0900

    main에서 로고 컴포넌트 적용

--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <NuxtLogo />
     <h1>메인 페이지</h1>
   </div>
 </template>
 
 <script>
-export default {}
+import NuxtLogo from '~/components/NuxtLogo.vue'
+export default {
+  components: {
+    NuxtLogo,
+  },
+}
 </script>
 
 <style></style>
