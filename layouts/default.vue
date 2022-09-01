3178137963f2907ca39658970a88bdf72a54802a
kj-dev <agassi77-dev@gmail.com>
2022-09-01 17:01:38 +0900

    default 레이아웃에 nuxt-link 적용

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>{{ $route.name }} 페이지</h1>
+    <header>
+      <h1>{{ $route.name }} 페이지</h1>
+      <NuxtLink to="/">홈페이지</NuxtLink>
+      <NuxtLink to="/main">메인페이지</NuxtLink>
+      <NuxtLink to="/product">상품페이지</NuxtLink>
+    </header>
     <Nuxt />
     <!-- <router-view></router-view> -->
   </div>
