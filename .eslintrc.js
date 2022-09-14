ca7b27d363381d6c293f8f58e96ff82cfd0b9aad
kj-dev <agassi77-dev@gmail.com>
2022-09-14 17:26:34 +0900

    eslint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,16 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
-  plugins: [],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    'prettier/prettier': 'error',
     'vue/multi-word-component-names': 0,
   },
 }
